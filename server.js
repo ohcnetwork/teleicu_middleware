3bf9b1353b4da6a4cb98970424310069858ca69d
Gigin George <mail.gigin@gmail.com>
2021-11-02 20:16:03 +0530

    Build POC

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-Stream = require('node-rtsp-stream')
-stream = new Stream({
-  name: 'name',
-  streamUrl: 'rtsp://admin:Lilo@123@192.168.1.64:554',
-  wsPort: 9999,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
-  }
+var
+  http = require('http'),
+  Cam = require('onvif').Cam;
+// var onvif = require('onvif');
+// onvif.Discovery.on('device', function(cam){
+// // function would be called as soon as NVT responses
+//     cam.username = "onvif_user";
+//     cam.password = "qwerty123";
+//     cam.connect(console.log);
+// })
+// onvif.Discovery.probe();
+
+var arg = Number(process.argv.slice(2));
+
+
+const express = require('express')
+const app = express()
+const port = 3010
+
+app.get('/', (req, res) => {
+new Cam({
+  hostname: "192.168.1.64",
+  username: "onvif_user",
+  password: "qwerty123"
+}, function(err) {
+  this.gotoPreset({
+    preset:1
+  },()=>{
+    console.log("Callback")
+  })
+});
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
