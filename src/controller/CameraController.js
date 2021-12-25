be98186eaba9c0f6a838f17eb71a98ef87998da0
developedBySJ <developedbysj@gmail.com>
2021-12-25 13:11:42 +0530

    Add camera controllers & routes

---
+++ src/controller/CameraController.js
@@ -0,0 +1,67 @@
+import { CameraUtils } from "../utils/CameraUtils.js";
+import { catchAsync } from "../utils/catchAsync.js";
+
+export class CameraController {
+  static _getCamParams = (body) => {
+    const { hostname, username, password, port } = body;
+
+    const camParams = {
+      useSecure: port === 443,
+      hostname,
+      username,
+      password,
+      port,
+    };
+
+    return camParams;
+  };
+
+  static gotoPreset = catchAsync(async (req, res) => {
+    const camParams = this._getCamParams(req.body);
+    const { preset } = req.body;
+
+    await CameraUtils.gotoPreset({ camParams, preset });
+
+    res.send({
+      status: "success",
+      message: `Camera preset is set to ${preset}.`,
+    });
+  });
+
+  static getPresets = catchAsync(async (req, res) => {
+    const camParams = this._getCamParams(req.body);
+    const presets = await CameraUtils.getPreset({ camParams });
+    res.send(presets);
+  });
+
+  static getStatus = catchAsync(async (req, res) => {
+    const camParams = this._getCamParams(req.body);
+    const status = await CameraUtils.getStatus({ camParams });
+
+    res.send(status);
+  });
+
+  static absoluteMove = catchAsync(async (req, res) => {
+    const camParams = this._getCamParams(req.body);
+    const { x, y, zoom } = req.body;
+
+    await CameraUtils.absoluteMove({ camParams, x, y, zoom });
+
+    res.send({
+      status: "success",
+      message: `Camera position updated!`,
+    });
+  });
+
+  static relativeMove = catchAsync(async (req, res) => {
+    const camParams = this._getCamParams(req.body);
+    const { x, y, zoom } = req.body;
+
+    await CameraUtils.relativeMove({ camParams, x, y, zoom });
+
+    res.send({
+      status: "success",
+      message: `Camera position updated!`,
+    });
+  });
+}
---
+++ src/utils/CameraUtils.js
@@ -0,0 +1,67 @@
+import * as onvif from "onvif";
+
+const Cam = onvif.Cam;
+
+export class CameraUtils {
+  constructor() {}
+
+  static gotoPreset = async ({ camParams, preset }) =>
+    new Promise((resolve, reject) => {
+      new Cam(camParams, function (err) {
+        if (err) return reject(err);
+
+        this.gotoPreset({ preset }, (data) => resolve(data));
+      });
+    });
+
+  static getPreset = async ({ camParams }) =>
+    new Promise(
+      (resolve, reject) =>
+        new Cam(camParams, function (err) {
+          if (err) return reject(err);
+
+          this.getPresets({}, (error, presets) => {
+            if (error) return reject(error);
+            if (presets) return resolve(presets);
+          });
+        })
+    );
+
+  static getStatus = async ({ camParams }) =>
+    new Promise(
+      (resolve, reject) =>
+        new Cam(camParams, function (err) {
+          if (err) return reject(err);
+          this.getStatus({}, (error, status) => {
+            if (error) return reject(error);
+            if (status) return resolve(status);
+          });
+        })
+    );
+
+  static absoluteMove = async ({ camParams, x, y, zoom }) =>
+    new Promise((resolve, reject) => {
+      new Cam(camParams, function (err) {
+        if (err) return reject(err);
+        try {
+          const result = this.absoluteMove({ x, y, zoom });
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+
+  static relativeMove = async ({ camParams, x, y, zoom }) =>
+    new Promise((resolve, reject) => {
+      new Cam(camParams, function (err) {
+        if (err) return reject(err);
+        try {
+          const result = this.relativeMove({ x, y, zoom });
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+}
