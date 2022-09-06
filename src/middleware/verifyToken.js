3c2e15fedbb63651f575cec928969ba28e8a4a39
pragati1610 <pragatibhattad1610@gmail.com>
2022-09-06 17:57:08 +0530

    feat: authorization middleware for MSE server

---
+++ src/middleware/verifyToken.js
@@ -0,0 +1,48 @@
+import jose from "node-jose";
+import jwktopem from "jwk-to-pem";
+import jwt from "jsonwebtoken";
+import axios from "axios";
+import { catchAsync } from "../utils/catchAsync.js";
+
+async function getKeyStore() {
+    const keyStore = await axios.get(
+        `http://localhost:9000/.well-known/openid-configuration`
+    );
+    console.log(JSON.stringify(keyStore.data));
+    return JSON.stringify(keyStore.data);
+}
+
+async function getPublicKey(jwks) {
+    const ks = await getKeyStore();
+    const keyStore = await jose.JWK.asKeyStore(ks);
+    const publicKey = keyStore.toJSON();
+    console.log("\n\n>>> publicKey: " + publicKey.__dict__);
+    return publicKey;
+}
+
+function getToken(tokenWithBearer) {
+    const bearerRequired = "Care_Bearer";
+    const tokenBearer = tokenWithBearer.split(" ")[0];
+    if (bearerRequired !== tokenBearer) {
+        throw new Error("Bearer is not authorized");
+    }
+    return tokenWithBearer.split(" ")[1];
+}
+
+export const verify = async(req, res, next) => {
+    try {
+        const tokenWithBearer = req.get("Authorization");
+        const token = getToken(tokenWithBearer);
+        const data = await getPublicKey();
+        const [firstKey] = data.keys;
+        const publicKey = jwktopem(firstKey);
+        req.claims = jwt.verify(token, publicKey);
+        next();
+    } catch (e) {
+        const error = e.message;
+        return res.status(403).send({
+            message: "jwt expected / jwt is invalid",
+            error,
+        });
+    }
+};
