f5ae31bc3dfd850d40ff5b2efe34cf13f05f8282
ishanExtreme <ishan2198@hotmail.com>
2023-06-30 23:19:58 +0530

    aws aded

--- src/controller/helper/makeDataDump.js
+++ src/controller/helper/makeDataDump.js
@@ -1,8 +1,17 @@
 import fs from 'fs'
+import aws from 'aws-sdk'
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
-export const makeDataDumpToJson = (v1Payload, v2Payload, assetExternalId, patient_id, consultation_id)=>{
+export const makeDataDumpToJson = async (v1Payload, v2Payload, assetExternalId, patient_id, consultation_id)=>{
 
   try{
+
+    const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+
     const dataDump = {
         "assetExternalId": assetExternalId,
         "patient_id": patient_id,
@@ -11,8 +20,22 @@ export const makeDataDumpToJson = (v1Payload, v2Payload, assetExternalId, patien
         "v2Payload": v2Payload
     }
 
-    const dataDumpJson = JSON.stringify(dataDump)
-    fs.writeFile(`../../../dump/${assetExternalId}.json`, dataDumpJson)
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: assetExternalId,
+      Body: JSON.stringify(dataDump)
+    };
+
+    await new Promise((resolve, reject) => {
+    s3.upload(params, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+    });
+
   }
   catch(err)
   {
