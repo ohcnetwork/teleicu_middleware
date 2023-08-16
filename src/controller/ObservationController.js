c27245d12457333087ee77f843d8734242e44646
khavinshankar <khavinshankar@gmail.com>
2023-08-16 17:27:34 +0530

    made s3 bucket dump cloud agnostic

--- src/controller/ObservationController.js
+++ src/controller/ObservationController.js
@@ -304,9 +304,14 @@ const updateObservationsToCare = async () => {
         console.log("updateObservationsToCare:cameraParams", cameraParams);
 
         const v2Payload = await updateObservationAuto(cameraParams, patient_id);
-        await makeDataDumpToJson(payload, v2Payload, asset.externalId, patient_id, consultation_id);
-      }
-      catch(err) {
+        await makeDataDumpToJson(
+          payload,
+          v2Payload,
+          asset.externalId,
+          patient_id,
+          consultation_id
+        );
+      } catch (err) {
         console.log("updateObservationsToCare:Data dump failed", err);
       }
       axios
